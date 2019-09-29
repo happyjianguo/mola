b1264129e52791bd325582887582f342466de3de
fengzao <542838596@qq.com>
2019-09-29 20:38:43 +0800

    新增对数据的处理

--- mola-core/src/main/java/org/cokebook/mola/spring/RepositoryBeanPostProcessor.java
+++ mola-core/src/main/java/org/cokebook/mola/spring/RepositoryBeanPostProcessor.java
@@ -10,13 +10,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.util.ReflectionUtils;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
- *
  * @date 2019/7/2 17:20
  */
 
@@ -42,7 +38,32 @@ public class RepositoryBeanPostProcessor implements ApplicationContextAware {
     }
 
     public class PropertiesInjector {
-        private final Set<Class> JAVA_WRAPPER_CLASSES = Sets.newHashSet(String.class, Short.class, Integer.class, Long.class, Byte.class, Character.class, Double.class, Float.class);
+
+        /**
+         * Java  基本类型包装类
+         */
+        private final Set<Class> JAVA_WRAPPER_CLASSES = Collections.unmodifiableSet(Sets.newHashSet(
+                Boolean.class, Short.class, Integer.class, Long.class, Byte.class, Character.class,
+                Double.class, Float.class
+        ));
+
+        /**
+         * Java 常用类: 该字段主要用于辅助做一些快速过滤
+         */
+        private final Set<Class> JAVA_COMMON_CLASSES = Collections.unmodifiableSet(Sets.newHashSet(
+                Class.class, String.class, Date.class, Object.class, StringBuffer.class, Thread.class,
+                Void.class, Package.class
+        ));
+        /**
+         * 基本类型集合
+         */
+        private final Set<Class> PRIMITIVE_TYPES = Collections.unmodifiableSet(Sets.newHashSet(
+                boolean[].class, byte[].class, char[].class, short[].class,
+                int[].class, long[].class, float[].class, double[].class,
+                boolean.class, byte.class, char.class, short.class,
+                int.class, long.class, float.class, double.class
+        ));
+
         private Set<Object> processed = new HashSet<>();
 
         public void inject(Object model) {
@@ -50,15 +71,46 @@ public class RepositoryBeanPostProcessor implements ApplicationContextAware {
                 return;
             }
             processed.add(model);
-            Class clazz = model.getClass();
-            if (clazz.isPrimitive() || JAVA_WRAPPER_CLASSES.contains(clazz)) {
+            final Class clazz = model.getClass();
+            if (PRIMITIVE_TYPES.contains(clazz) ||
+                    JAVA_WRAPPER_CLASSES.contains(clazz) ||
+                    JAVA_COMMON_CLASSES.contains(clazz)) {
                 return;
             }
+            handleOnArray(model);
+            handleOnCollection(model);
+            handleOnMap(model);
+            handleOnModel(model);
+        }
+
+        private void handleOnModel(Object model) {
+            if (model.getClass().isAnnotationPresent(Model.class)) {
+                applicationContext.getAutowireCapableBeanFactory().autowireBean(model);
+                ReflectionUtils.doWithFields(model.getClass(), (field) -> {
+                    field.setAccessible(true);
+                    inject(field.get(model));
+                });
+            }
+        }
+
+        private void handleOnArray(Object model) {
+            if (model.getClass().isArray() || !PRIMITIVE_TYPES.contains(model.getClass())) {
+                Object[] modelArray = (Object[]) model;
+                for (Object item : modelArray) {
+                    inject(item);
+                }
+            }
+        }
+
+        private void handleOnCollection(Object model) {
             if (model instanceof Collection) {
                 for (Object obj : (Collection) model) {
                     inject(obj);
                 }
             }
+        }
+
+        private void handleOnMap(Object model) {
             if (model instanceof Map) {
                 for (Object key : ((Map) model).keySet()) {
                     inject(key);
@@ -66,13 +118,6 @@ public class RepositoryBeanPostProcessor implements ApplicationContextAware {
                 for (Object value : ((Map) model).values()) {
                     inject(value);
                 }
-            }
-            if (model.getClass().isAnnotationPresent(Model.class)) {
-                applicationContext.getAutowireCapableBeanFactory().autowireBean(model);
-                ReflectionUtils.doWithFields(model.getClass(), (field) -> {
-                    field.setAccessible(true);
-                    inject(field.get(model));
-                });
             }
         }
 
